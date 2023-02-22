c27be10af80c9d4166b44b7e77067bdd6351f928
João Vitor <joaovitorcfdev@gmail.com>
2023-02-22 15:48:41 -0300

    criando telas

---
+++ src/components/List/index.tsx
@@ -0,0 +1,38 @@
+import Item from './Item';
+import style from './List.module.scss';
+
+function List() {
+  const tarefas = [
+    {
+      tarefa: 'React',
+      tempo: '02:00:00',
+    },
+    {
+      tarefa: 'JavaScript',
+      tempo: '03:30:00',
+    },
+    {
+      tarefa: 'JavaScript',
+      tempo: '03:30:00',
+    },
+    {
+      tarefa: 'JavaScript',
+      tempo: '03:30:00',
+    }
+  ];
+
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {
+          tarefas.map((item, index) => (
+            <Item key={index} {...item} />
+          ))
+        }
+      </ul>
+    </aside>
+  );
+}
+
+export default List;
